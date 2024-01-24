30fc1c17ec827390bd1449dcd03ca0057cebcc3d
Marisol Castillo <36350945@fi.unju.edu.ar>
2024-01-24 02:53:38 -0300

    Inicio del proyecto

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('node:path');
+
+
+app.listen( 3001, () => console.log(`Server up: http://localhost:3001`) )
+
+
+app.get( '/', ( req,res ) =>{
+    const pathHome = path.join(__dirname, 'views/home.html')
+    res.sendFile(pathHome);
+} )
